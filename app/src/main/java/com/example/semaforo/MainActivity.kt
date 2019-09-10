fbd8b2435f1d225623317c43d329ee763863fa1e
Osegueda <moisesfreestyler@gmail.com>
2019-09-10 15:16:22 -0600

    update position

--- app/src/main/java/com/example/semaforo/MainActivity.kt
+++ app/src/main/java/com/example/semaforo/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Layout
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -13,14 +15,16 @@ import java.util.TimerTask
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    //inicializo una variable con la clase handler
     lateinit var updateHandler: Handler
     var estado = -1
+    var state: TextView?=null
+    var background: View?=null
 
-
+    //aqui se ejecutara mi funcion cada 5 segundos
     private val updateTextTask = object : Runnable {
         override fun run() {
-            prueba()
+            prueba()//funcion para cambiar colores
             updateHandler.postDelayed(this, 5000)
         }
     }
@@ -29,23 +33,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //le devuelve un bucle
         updateHandler = Handler(Looper.getMainLooper())
 
-         val background1= findViewById<LinearLayout>(R.id.backgroundLayout)
+        //id del layout
+         background= findViewById<LinearLayout>(R.id.backgroundLayout)
 
+        //envento onclcick del BOTON para iniciar el semaforo
         val btn = findViewById<Button>(R.id.boton)
         btn.setOnClickListener {view->
 
             if (estado == -1){
+                //ejecuta el la funcion
                 updateHandler.post(updateTextTask)
                 estado = 0
                 btn.setText("parar")
 
             }else{
+                //detiene la funcion
                 updateHandler.removeCallbacks(updateTextTask)
                 estado = -1
+                state!!.setText("Deshabilitado")
                 btn.setText("Iniciar")
-                background1.setBackgroundColor(Color.GRAY)
+                background!!.setBackgroundColor(Color.GRAY)
             }
 
 
@@ -53,24 +63,27 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-
+    //FUNCION PARA CAMBIAR LOS COLORES DEL FONDO CON WHEN
     private fun prueba(){
-        val background = findViewById<LinearLayout>(R.id.backgroundLayout)
-        val state= findViewById<TextView>(R.id.textview)
+        //guarda el id del layout
+        //val background = findViewById<LinearLayout>(R.id.backgroundLayout)
+        //Guarda el id para setear el textview a los diferentes estados
+        state= findViewById<TextView>(R.id.textview)
         when (estado){
             0 ->{
-                state.setText("Adelante")
-                background.setBackgroundColor(Color.GREEN)
+
+                state!!.setText("Alto")
+                background!!.setBackgroundColor(Color.RED)
                 estado += 1
             }
             1 ->{
-                state.setText("Precaucion")
-                background.setBackgroundColor(Color.YELLOW)
+                state!!.setText("Precaucion")
+                background!!.setBackgroundColor(Color.YELLOW)
                 estado += 1
             }
             2 ->{
-                state.setText("Alto")
-                background.setBackgroundColor(Color.RED)
+                state!!.setText("Adelante")
+                background!!.setBackgroundColor(Color.GREEN)
                 estado = 0
             }
         }
